1e1d679869922cfb11a9aca63d3eee0f3ca01855
YaminiSuman <me.yamini.01@gmail.com>
2023-01-23 16:51:03 +0000

    setup form, fix file path issue

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,7 +1,17 @@
-import PlaceForm from "../components/Places/PlaceForm";
+// import PlaceForm from "../components/Places/PlaceForm";
 
-function AddPlace() {
-  return <PlaceForm />;
+// function AddPlace() {
+//   return <PlaceForm />;
+// }
+
+// export default AddPlace;
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AddPlace() {
+  return (
+    <View>
+      <Text>AddPlace</Text>
+    </View>
+  )
 }
-
-export default AddPlace;
---
+++ screens/AllPlaces.js
@@ -0,0 +1,16 @@
+// import PlacesList from "../components/Places/PlacesList";
+
+// function AllPlaces() {
+//   return <PlacesList/>;
+// }
+
+// export default AllPlaces;
+import { View, Text } from 'react-native'
+import React from 'react'
+import PlacesList from '../components/places/PlacesList'
+
+export default function AllPlaces() {
+  return (
+    <PlacesList/>
+  )
+}
