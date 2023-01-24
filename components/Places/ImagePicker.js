8994847e2597c6b2ce080935d7d0a6f4bc5d3763
YaminiSuman <me.yamini.01@gmail.com>
2023-01-24 10:09:03 +0000

    create custom button

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -7,6 +7,7 @@ import {
 import { useState } from "react";
 
 import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
   const [pickedImage, setPickedImage] = useState();
@@ -57,7 +58,9 @@ function ImagePicker() {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 }
